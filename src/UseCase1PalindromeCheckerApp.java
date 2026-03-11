509d8d04c7630062cba682929cc6d61660b8d299
vijay-Raghv <dthiyagarajan64@gmail.com>
2026-03-11 09:19:29 +0530

    Updated UseCase1PalindromeCheckerApp

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==========================================");
+        System.out.println(" Welcome to the Palindrome Checker System ");
+        System.out.println("==========================================");
+
+        System.out.println("Version : 1.0");
+
+        System.out.println("System initialized successfully.");
+
+    }
+}
